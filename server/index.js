4f7b037314da78ca0bdcaf162304685036ef9301
ginpei <ginpei@gmail.com>
2021-07-30 01:09:00 +0000

    feat: 🎸 home page HTML

--- server/index.js
+++ server/index.js
@@ -3,9 +3,10 @@ const express = require("express");
 const port = 3000;
 
 const app = express();
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.json({ ok: true });
+  res.render("./index.ejs");
 });
 
 app.listen(port, () => {
