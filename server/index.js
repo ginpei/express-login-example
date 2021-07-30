6cc0ea49a388a0ca1c7d8b0e4d23745ee0479081
ginpei <ginpei@gmail.com>
2021-07-30 01:15:07 +0000

    feat: 🎸 login page

--- server/index.js
+++ server/index.js
@@ -6,7 +6,20 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("./index.ejs");
+  res.render("index.ejs");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login/index.ejs");
+});
+
+app.post("/login", (req, res) => {
+  // WIP
+
+  console.log("# req.body", req.body);
+
+  res.status(500);
+  res.json({ ok: false, message: "Not implemented" });
 });
 
 app.listen(port, () => {
